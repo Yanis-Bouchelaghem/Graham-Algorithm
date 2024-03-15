4ca253cd16ba81acc01ddbb6eb0e3dcd866da129
Yanis Bouchelaghem <bouchelaghemyanis7@gmail.com>
2024-03-15 11:13:59 +0100

    Brought in Game, Vec2 and settings files from another one of my projects.

--- Graham-Algorithm/main.cpp
+++ Graham-Algorithm/main.cpp
@@ -1,6 +1,17 @@
-
+#include "Game.h"
+#include "Settings.h"
 
 int main()
 {
+
+    Game game{settings::screenWidth,
+              settings::screenHeight,
+              settings::fps,
+              "Tetris Raylib" };
+
+    while (!game.GameShouldClose())
+    {
+        game.Tick();
+    }
 	return 0;
 }
